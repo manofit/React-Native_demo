8c1a6a2be7bc4e59220486be5ff8fe5d3f62cc35
manofit <gaojun52ioskf@163.com>
2018-05-02 10:21:33 +0800

    创建news项目

---
+++ News_Pro/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
